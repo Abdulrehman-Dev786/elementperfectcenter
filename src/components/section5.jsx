c1ce787f85198080e02be93486f0e89e8a2b22c6
hammad <hammad>
2025-09-15 11:11:29 +0500

    fix: updated

--- src/components/section5.jsx
+++ src/components/section5.jsx
@@ -8,11 +8,11 @@ export function Section5(props) {
           <h1 className="text-[90px] text-blue-900 font-extrabold">140 +</h1>
           <p className="text-3xl text-blue-900 font-bold">Clients Dialed</p>
         </div>
-        <div className="flex flex-col h-48 items-center justify-center  hover:bg-orange-500 *:hover:text-white border-blue-900 border-2 ">
+        <div className="flex flex-col h-48 items-center justify-center  hover:bg-orange-400 *:hover:text-white border-blue-900 border-2  transition-all  duration-700 ease-in-out  ">
           <h1 className="text-[90px] text-blue-900 font-extrabold ">25k</h1>
           <p className="text-3xl text-blue-900 font-bold">Successsful Leads</p>
         </div>
-        <div className="flex flex-col h-48 items-center  hover:bg-orange-500 *:hover:text-white justify-center border-blue-900 border-2">
+        <div className="flex flex-col h-48 items-center  hover:bg-orange-400 *:hover:text-white justify-center border-blue-900 border-2  transition-all  duration-700 ease-in-out ">
           <h1 className="text-[90px] text-blue-900 font-extrabold">75%</h1>
           <p className="text-3xl text-blue-900 font-bold">Contact Rate</p>
         </div>
