1f549906493bff06b4e257a851a3c412b33125c6
hammad <hammad>
2025-09-15 10:48:58 +0500

    fix: updated

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,24 +2,18 @@ import React from "react";
 
 const Sidebar = ({ sections, activeId, onSelect }) => {
   return (
-    <div className="grid grid-cols-1 md:w-1/4 bg-white md:border-r md:min-h-screen">
+    <div className="flex w-full  md:w-1/4 bg-white border-r md:min-h-screen">
       <ul>
         {sections.map((section) => (
           <li
             key={section.id}
-            className={`ps-3 w-full md:w-[337px] py-3 cursor-pointer hover:bg-orange-100 transition relative
-              ${
-                activeId === section.id
-                  ? "bg-orange-100 text-orange-600 font-semibold"
-                  : ""
-              }`}
+            className={`ps-3 w-[395px] md:w-[337px] py-3 cursor-pointer hover:bg-gray-100 transition ${
+              activeId === section.id ? "bg-orange-100 text-orange-600 border-r-2 border-orange-600 font-semibold" : ""
+            }`}
             onClick={() => onSelect(section.id)}
           >
-            {/* Active Orange Line */}
-            {activeId === section.id && (
-              <span className="absolute left-0 top-0 h-full w-1 bg-orange-600 rounded-r-md transition-all duration-300"></span>
-            )}
             {section.title}
+            
           </li>
         ))}
       </ul>
