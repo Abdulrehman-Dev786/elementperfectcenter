8048c458da1afaa05281a93197e9dc9ed87206d4
hammad <hammad>
2025-09-15 11:05:28 +0500

    fix: updated

--- src/components/section5.jsx
+++ src/components/section5.jsx
@@ -4,7 +4,7 @@ export function Section5(props) {
   return (
     <>
       <div className="grid grid-cols-1  md:grid-cols-3 *:font-serif gap-3 p-2">
-        <div className="flex flex-col h-48 items-center  hover:bg-orange-500 *:hover:text-white justify-center border-blue-900 border-2 ">
+        <div className="flex flex-col h-48 items-center  hover:bg-orange-500 *:hover:text-white transition-all justify-center border-blue-900 border-2 ">
           <h1 className="text-[90px] text-blue-900 font-extrabold">140 +</h1>
           <p className="text-3xl text-blue-900 font-bold">Clients Dialed</p>
         </div>
